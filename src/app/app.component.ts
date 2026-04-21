d85dfa337a70a7aa6a866d9e1a8aa33895356ee8
wesleyfelix <wesley.felixdelima96@hotmail.com>
2026-04-21 16:17:28 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'passeio-app';
